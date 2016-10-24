8c359b95485df6ce9a5d4b1418fd368257277beb
wada yuki <ywada@glodia.jp>
2016-10-24 17:59:16 +0900

    大体決定稿

--- dir1.rb
+++ dir1.rb
@@ -1,14 +1,14 @@
-require 'find'
- File.open("/Users/glodia/desktop/list.txt","w+")
-include Find
-find('\Users\glodia\rubyfiles'){|fl| 
-    if fl.include?("test")
-        File.open("/Users/glodia/desktop/list.txt","a") {|f|
-        f.puts fl
-            }
-        else
-        File.open("/Users/glodia/desktop/list.txt","a") {|f|
-        f.print("対象ファイルではありません\n")
-            }
-    end
-    }
+
+bar = []
+bar2 = []
+File.open('C:\Users\glodia\Desktop\test\testfile1.txt', 'r:utf-8') do |f|
+  f.each_line do |line|
+      bar.push(line.clone)
+  end
+end
+File.open('C:\Users\glodia\Desktop\test\testfile2.txt','r:utf-8') do |f|
+  f.each_line do |line|
+      bar2.push(line.clone)
+  end
+end
+puts(bar.include?(bar2))
